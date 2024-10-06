021d1218dea8cbefaebca0cf39eaee70215ff040
Anilrathodnew <rathod.anilk06@gmail.com>
2024-10-06 23:27:25 +0530

    All the file added

---
+++ src/Oct/ex_04102024/Lab005.java
@@ -0,0 +1,11 @@
+package Oct.ex_04102024;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+import java.net.StandardSocketOptions;
+
+public class Lab005 {
+    public static void main(String[] args) {
+        System.out.println("Lab005");
+
+    }
+}
