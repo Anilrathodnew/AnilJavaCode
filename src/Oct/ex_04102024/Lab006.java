926477cde392c6e1a22ad71c201793dc99dfa8de
Anilrathodnew <rathod.anilk06@gmail.com>
2024-10-18 00:28:15 +0530

    Code Added for 17 Oct 2024

--- src/Oct/ex_04102024/Lab006.java
+++ src/Oct/ex_04102024/Lab006.java
@@ -3,6 +3,8 @@ package Oct.ex_04102024;
 public class Lab006 {
     public static void main(String[] args) {
         System.out.println(2+4);
-        System.out.println("0");
+        System.out.println(5-2);
+        System.out.println(5*4);
+        System.out.println(4/2);
     }
 }
