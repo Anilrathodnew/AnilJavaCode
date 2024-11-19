b884a1c7eb1963f151d6138e7220eebcf3467761
Anilrathodnew <rathod.anilk06@gmail.com>
2024-11-19 20:33:19 +0530

    Code Added for Lab019 Oct 2024 code

---
+++ src/Oct/ex_04102024_Main_Method/Lab006_main_Arguments.java
@@ -0,0 +1,6 @@
+package Oct.ex_04102024_Main_Method;
+
+public class Lab006_main_Arguments {
+    public static void main(String[] anil) {
+    }
+}
---
+++ src/Oct/ex_04102024_Main_Method/Lab007_Hello_world.java
@@ -0,0 +1,7 @@
+package Oct.ex_04102024_Main_Method;
+
+public class Lab007_Hello_world {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
